05a3a9c3b0e794776ec487c1533c348d93472159
Ryan Shields <ryandennisshields@gmail.com>
2024-02-13 12:14:39 +0000

    Sword: Swiping upward and occlusion
    Swiping upward to collect the sword, occlusion updates on the sword only

--- Assets/Scripts/Generic/Triggered.cs
+++ Assets/Scripts/Generic/Triggered.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -11,6 +12,7 @@ namespace GCU.CultureTour
         public UnityEvent<GameObject, Collider> TriggerEntered;
         public UnityEvent<GameObject, Collider> TriggerExited;
         public UnityEvent<GameObject> Tapped;
+        public UnityEvent<GameObject> Swiping;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -30,12 +32,40 @@ namespace GCU.CultureTour
             }
         }
 
+        // Tapping
         private void OnMouseUp()
         {
             Tapped?.Invoke(gameObject);
             if (EnableDebug)
             {
                 Debug.Log($"Mouse Up event called on {gameObject.name}.", gameObject);
+            }
+        }
+
+        private Vector2 startTouchPos;
+        private Vector2 endTouchPos;
+
+        // Swiping Upward
+        private void Update()
+        {
+            float swipeDistance = 5f;
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                startTouchPos = Input.GetTouch(0).position;
+            }
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                endTouchPos = Input.GetTouch(0).position;
+
+                if (endTouchPos.y > startTouchPos.y + swipeDistance)
+                {
+                    Swiping?.Invoke(gameObject);
+                    if (EnableDebug) 
+                    {
+                        Debug.Log($"Swipe upward event called on {gameObject.name}.", gameObject);
+                    }
+                }
             }
         }
 
