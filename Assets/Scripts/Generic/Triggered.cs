0dd68013d23bf14dbedf996b6afa6462cb71dd67
Ryan Shields <ryandennisshields@gmail.com>
2024-02-24 19:46:02 +0000

    Interaction updates (needs fixing)
    Drag and drop moved to Triggered code
    Swiping now works in modular directions
    Hold now works
    
    Drag and drop + swiping need fixing, negative values for the desired position don't work

---
+++ Assets/Scripts/Generic/Triggered.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GCU.CultureTour
+{
+    public class Triggered : MonoBehaviour
+    {
+        public GameObject _object;
+        public bool EnableDebug;
+        public UnityEvent<GameObject, Collider> TriggerEntered;
+        public UnityEvent<GameObject, Collider> TriggerExited;
+        public UnityEvent<GameObject> Tapped;
+        [SerializeField]
+        [Tooltip("Enable this if you also want to move the object with the player's swipe interaction. Desired position will change where the object needs to be to count as collected.")]
+        private bool _dragObject;
+        [SerializeField]
+        [Tooltip("X and Y (and Z) values change what direction the player needs to swipe/move, e.g. a positive Y value means an upward swipe/move.\nThe higher the value, the further the player needs to swipe/move to collect the object.\nZ value is only used if drag object is enabled!")]
+        private Vector3 _desiredPosition;
+        public UnityEvent<GameObject> Swiping;
+        [SerializeField]
+        [Tooltip("Duration the player needs to hold the object.")]
+        private float _holdDuration;
+        public UnityEvent<GameObject> Holding;
+
+        private bool _isHolding;
+        private Vector3 _holdPosition;
+        private Vector3 _startPosition;
+        private Vector3 _newPosition;
+        private float _holdTimer;
+
+        private void Start()
+        {
+            _startPosition = _object.transform.position;
+            _holdTimer = _holdDuration;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerEntered?.Invoke(gameObject, other);
+            if (EnableDebug)
+            {
+                Debug.Log($"{other.gameObject.name} triggered {gameObject.name}.", gameObject);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            TriggerExited?.Invoke(gameObject, other);
+            if (EnableDebug)
+            {
+                Debug.Log($"{other.gameObject.name} exited the trigger collider of {gameObject.name}.", gameObject);
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            _holdPosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            _isHolding = true;
+        }
+
+        // Tapping
+        private void OnMouseUp()
+        {
+            _isHolding = false;
+            _holdTimer = _holdDuration;
+            Tapped?.Invoke(gameObject);
+            if (EnableDebug)
+            {
+                Debug.Log($"Mouse Up event called on {gameObject.name}.", gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            if (_isHolding)
+            {
+                _newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition - _holdPosition);
+                _newPosition.x = Mathf.Clamp(_newPosition.x, _startPosition.x, _startPosition.x + _desiredPosition.x);
+                _newPosition.y = Mathf.Clamp(_newPosition.y, _startPosition.y, _startPosition.y + _desiredPosition.y);
+                _newPosition.z = Mathf.Clamp(_newPosition.z, _startPosition.z, _startPosition.z + _desiredPosition.z);
+
+                if (_dragObject)
+                {
+                    _object.transform.position = _newPosition;
+                }
+
+                _holdTimer -= Time.deltaTime;
+
+                if (_holdTimer <= 0)
+                {
+                    Holding?.Invoke(gameObject);
+                    if (EnableDebug)
+                    {
+                        Debug.Log($"Hold event called on {gameObject.name}.", gameObject);
+                    }
+                }
+            }
+
+            if (_newPosition == _desiredPosition + _startPosition)
+            {
+                Swiping?.Invoke(gameObject);
+                if (EnableDebug)
+                {
+                    Debug.Log($"Swipe event called on {gameObject.name}.", gameObject);
+                }
+            }
+        }
+    }
+}
